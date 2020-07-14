9dac617d8872ab0a73d433fbcdb28937c29b0179
oclean66 <oclean66@gmail.com>
2020-07-14 01:10:13 -0400

    mail

--- protected/config/main.php
+++ protected/config/main.php
@@ -6,7 +6,7 @@
 // CWebApplication properties can be configured here.
 return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
-    'name' => 'Hefestos',
+    'name' => 'Hefestos, Inventario y control',
     'language' => 'es_es',
     // 'timeZone' => 'America/Caracas',
     'sourceLanguage' => 'es',
