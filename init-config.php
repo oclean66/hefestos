86d3ee95e9ef2d5d9a6c5702aca054f553301429
Jaylerth <oclean66@gmail.com>
2021-03-20 02:38:51 -0400

    done

--- init-config.php
+++ init-config.php
@@ -1,9 +1,19 @@
 <?php
- 
 $master = array(
-    'connectionString' => 'mysql:host=localhost;dbname=instance_database',
-    'emulatePrepare' => true,
-    'username' => 'username',
-    'password' => 'password',
-    'charset' => 'utf8',
+    'database'=> array(
+        'connectionString' => 'mysql:host=localhost;dbname=hefestos',
+        'emulatePrepare' => true,
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ), 
+    'crugemailer'=> array(
+        'class' => 'application.modules.crugemailer.CrugeSwiftMailer',
+        'mailfrom' => 'soporte.kingdeportes@gmail.com',
+        'subjectprefix' => 'Notificacion - ',
+        'transport' => 'gmail', //imap, pop3
+        'gmailAcount' => "company@gmail.com",
+        'gmailPassword' => "password",       
+        // 'debug' => true,
+    )
 );
